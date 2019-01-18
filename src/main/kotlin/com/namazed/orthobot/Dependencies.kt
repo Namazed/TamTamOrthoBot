fe26139f547da53bc727dd048f159270ac890f8d
alexander.melnikov <alexander.melnikov@corp.mail.ru>
2019-01-19 00:23:57 +0300

    Deleted ProdApiKeys

--- src/main/kotlin/com/namazed/orthobot/Dependencies.kt
+++ src/main/kotlin/com/namazed/orthobot/Dependencies.kt
@@ -1,8 +1,7 @@
 package com.namazed.orthobot
 
-import com.namazed.orthobot.client.ProdApiKeys
 import org.koin.dsl.module.module
 
 val serverModule = module {
-    single<ApiKeys> { ProdApiKeys() }
+//    single<ApiKeys> { }
 }
